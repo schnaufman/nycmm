0a088255e3054746b41dcdaa75f9f484322ffb39
matt-hires <m.mairleitner@gmail.com>
2019-06-17 12:53:08 +0200

    * first try with the mobile menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,23 @@ Foundation.plugin(SmoothScrollWithLinks, 'SmoothScrollWithLinks');
 // --------------------------------------------------
 $(document).foundation();
 
-// initialize custom apis
+// initialize custom apis and mobile navigation
 $(function () {
   new LeafletApi('leafletMap');
+
+  // mobile navigation
+  $('.js--mobile-nav-icon').click(function () {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--mobile-nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-ios-reorder')) {
+      icon.removeClass('ion-ios-reorder');
+      icon.addClass('ion-ios-close');
+    } else {
+      icon.removeClass('ion-ios-close');
+      icon.addClass('ion-ios-reorder');
+    }
+  });
 });
+
