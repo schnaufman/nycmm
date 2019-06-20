dc81ad7788642ebb57a56e9a1203a5f8462c3645
matt-hires <m.mairleitner@gmail.com>
2019-06-20 09:13:40 +0200

    * introduced nav handler class * more space in header items

---
+++ assets/js/nav-handler.js
@@ -0,0 +1,19 @@
+'use strict';
+import $ from 'jquery';
+
+/**
+ * navigation handler
+ *
+ * should hide and show navigation menus
+ */
+class NavHandler {
+
+  /**
+   * Construct navigation handler
+   */
+  constructor() {
+    this.iconMenuOpen = 'ion-ios-menu';
+    this.iconMenuClose = 'ion-md-menu';
+  }
+}
+export { NavHandler };
