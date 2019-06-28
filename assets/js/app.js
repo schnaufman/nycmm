de3e5d46110e07009932d7cc8cc01f32ac396d7e
matt-hires <m.mairleitner@gmail.com>
2019-06-28 08:53:29 +0200

    * Performance improvements for js loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,28 @@
 // --------------------------------------------------
 // APP.JS
 // --------------------------------------------------
+'use strict';
 import $ from 'jquery';
 import { Foundation } from './lib/foundation-explicit-pieces';
 import { SmoothScrollWithLinks } from './lib/ext/foundation.smoothScrollWithLinks';
 import { LeafletApi } from './lib/maps/leaflet-api';
-import { NavHandler } from './nav-handler';
-import { MobileHelper } from './mobile-helper';
+import { NavHandler } from './lib/nav-handler';
+import { MobileHelper } from './lib/mobile-helper';
 
 //
 // Custom JS
 // --------------------------------------------------
 Foundation.plugin(SmoothScrollWithLinks, 'SmoothScrollWithLinks');
 
+// init foundation js
+$(document).foundation();
+
 // initialize custom apis and mobile navigation
 $(function () {
-  // cache jQuery object for window.document
-  var $document = $(document);
+  MobileHelper.centerLeafletMapMarkerOnMobile('leafletMap');
 
-  // init foundation js
-  $document.foundation();
+  new LeafletApi('leafletMap');
+  new NavHandler('js--dropdown-nav-menu', 'js--dropdown-nav-icon');
 
-  // other stuff
-  $document.ready(function () {
-    MobileHelper.centerLeafletMapMarkerOnMobile('leafletMap');
-
-    new LeafletApi('leafletMap');
-    new NavHandler('js--dropdown-nav-menu', 'js--dropdown-nav-icon');
-
-    MobileHelper.disableVideoAutoplayOnMobile();
-
-  });
-
+  MobileHelper.disableVideoAutoplayOnMobile();
 });
