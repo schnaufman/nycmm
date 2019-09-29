1f4d4cb09fc98ed4622d9e591d9c36be5f3c3b41
matt-hires <m.mairleitner@gmail.com>
2019-09-29 15:14:43 +0200

    [content] [libs] added gallery draft and started implementing photoswipeapi

--- assets/js/lib/gallery/photoswipe-api.js
+++ assets/js/lib/gallery/photoswipe-api.js
@@ -10,10 +10,47 @@ import { PhotoSwipeUI_Default } from 'photoswipe/dist/photoswipe-ui-default';
 class PhotoSwipeApi {
 
   /**
-   * contruct and intiialize photoswipe api
+   * Constructs and intializes PhotoSwipeAPI
    *
+   * @param {String} Gallery element class in the DOM
    */
-  constructor() {
+  constructor(galleryElClass) {
+    const galleryItems = this._parseThumbnail(galleryElClass);
+
+    console.debug(`PhotoSwipeApi: Parsed thumbnails and created gallery objects: ${galleryItems}`);
+  }
+
+  _parseThumbnail(galleryElClass) {
+    // parse slide data (url, title, size ...) from DOM elements
+    // (children of gallerySelector)
+    const $thumbElements = $('.' + galleryElClass + ' > figure');
+
+    const items = $thumbElements
+      .map(($figureEl) => {
+        const $linkEl = $figureEl.children('a').first();
+        const itemSize = $linkEl.getAttribute('data-size').split('x');
+        const itemHref = $linkEl.getAttribute('href');
+
+        // thumbnail element
+        const $linkImgEl = $linkEl.children('img').first();
+        const itemThumbnailUrl = $linkImgEl ? $linkImgEl.getAttribute('src') : null;
+
+        // item title
+        const $figcaptionEl = $figureEl.children('figcaption').first();
+        const itemTitle = $figcaptionEl ? $figcaptionEl.getHtml() : null;
+
+        //create slide object
+        return {
+          src: itemHref,
+          w: parseInt(itemSize[0], 10),
+          h: parseInt(itemSize[1], 10),
+          title: itemTitle,
+          msrc: itemThumbnailUrl,
+          el: $figureEl
+        }
+      })
+      .get();
+    return items;
   }
 
 }
