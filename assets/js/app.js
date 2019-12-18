f2d6a8a6f4e3d037253b2713c63f6b432eaa9cf3
matt-hires <m.mairleitner@gmail.com>
2019-12-18 09:08:46 +0100

    [bug] try other init order

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,39 +20,38 @@ AOS.init({
 
 Foundation.plugin(SmoothScrollWithLinks, 'SmoothScrollWithLinks');
 
-// scroll to location if passed by session storage
-SmoothScrollWithLinks.initSessionNavScrollHash('content');
-
 // lazyload images
 // eslint-disable-next-line no-undef
 lazyload();
 
+// scroll to location if passed by session storage
+SmoothScrollWithLinks.initSessionNavScrollHash('content');
+
 // map init - currently there's the limitation to have exactly ONE gmapsMap Element in the DOM
 const gmapsApi = new GMapsApi('${NYCMM_ENV_GMAPS_API_KEY}', 'gmapsMap');
 
-// FIREFOX/SAFARI MOBILE SCROLL Bugfix with 100vh and navigation bar included in css height
-// force header height to window size (fix for firefox mobile scroll)
-$('#site-header').height($(window).height());
-
-new NavHandler(
-  'js--nav-dropdown',
-  'js--nav-dropdown-icon',
-  'js--nav-dropdown-item-section-dropdown',
-  'js--nav-dropdown-item-section-dropdown-icon',
-  'js--nav-container-sticky',
-  'js--nav-back-to-top-container');
-
 new CookieConsentHelper('${NYCMM_ENV_GOOGLE_TRACKING_ID}');
 
-gmapsApi.initialize();
-
-// init gallery AFTER lazyload
 new GalleriaApi('galleria');
 
 // initialize custom apis and mobile navigation
 $(document).ready(function () {
   // init foundation js
   $(document).foundation();
+
+  // FIREFOX/SAFARI MOBILE SCROLL Bugfix with 100vh and navigation bar included in css height
+  // force header height to window size (fix for firefox mobile scroll)
+  $('#site-header').height($(window).height());
+
+  new NavHandler(
+    'js--nav-dropdown',
+    'js--nav-dropdown-icon',
+    'js--nav-dropdown-item-section-dropdown',
+    'js--nav-dropdown-item-section-dropdown-icon',
+    'js--nav-container-sticky',
+    'js--nav-back-to-top-container');
+
+  gmapsApi.initialize();
   // when using formspree
   // const contactMailParts = '${NYCMM_ENV_CONTACT_MAIL}'.split('@');
   // const mailAdress = contactMailParts[0];
